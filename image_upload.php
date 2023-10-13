60df51af83b18f2b99f5c22cc974d065be88e87a
Md. Saiful Islam <82088025+creativesaiful@users.noreply.github.com>
2023-10-13 13:44:11 +0600

    Update image_upload.php

--- image_upload.php
+++ image_upload.php
@@ -38,43 +38,57 @@ $multi_images = $request->file('multi_images');
 <?php
  function ProfileUpdate(Request $request){
 
+      
         $validate = $request->validate([
             'name' => 'required',
             'phone' => 'required || min:11 || max:11',
             'default_currency'=>'required',
-            'profile_image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            'profile_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            
         ]);
-
 
         
         $name = $request->name;
         $phone = $request->phone;
         $default_currency= $request->default_currency;
+        $user = User::where('id', auth()->user()->id)->first();
 
         //profile image procesing
-        if ($request->file('profile_image')) {
+        if ($request->profile_image) {
             $image = $request->file('profile_image');
             $imageName = time() . '.' . $image->getClientOriginalExtension();
             $image->move(public_path('storage/uploads'), $imageName); 
+
+            unlink('storage/uploads/'.$user->profile_photo_path);
+        }else{
+            $imageName = $user->profile_photo_path;
         }
 
-
-
+      try{
         $user = User::where('id', auth()->user()->id)->first();
         $user->name = $name;
         $user->phone = $phone;
         $user->default_currency = $default_currency;
-
-        if($imageName){
-            $user->profile_photo_path = $imageName;
-        }
-        
+        $user->profile_photo_path = $imageName; 
         $user->save();
 
-        return redirect()->route('profile')->with('success', 'Profile Updated');
+        $success = [
+            'type' => 'success',
+            'message'=>'Profile Updated Successfully',
+        ];
+
+        return redirect()->route('profile')->with($success);
+      }catch(\Exception $e){
+        $error = [
+            'type' => 'error',
+            'message'=>$e->getMessage(),
+        ];
+
+        return redirect()->route('profile')->with($error);
 
 
        
+    }
     }
 
 // View Image in blade file
